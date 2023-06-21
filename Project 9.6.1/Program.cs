f219bf043e8ec3a01e4775f05f232a4c763907b9
VS_KUZNEC <sevactet.games@gmail.com>
2023-06-21 11:36:56 +0300

    Добавьте файлы проекта.

---
+++ Project 9.6.1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Exception[] exceptions = new Exception[]
+            {
+                new MyException(),
+                new ArgumentNullException(),
+                new ArgumentOutOfRangeException(),
+                new DirectoryNotFoundException(),
+                new Exception(),
+            };
+            try
+            {
+                new ArgumentNullException();
+                new ArgumentOutOfRangeException();
+                new DirectoryNotFoundException();
+                new Exception();
+                throw new MyException();
+            }
+            catch (MyException ex)
+            {
+                Console.WriteLine("Материал пройден, но осталось закрепить практикой.");
+            }
+
+            catch (DirectoryNotFoundException ex)
+            {
+                Console.WriteLine("Произошла ошибка номер 33. Попробуйте снова.");
+            }
+
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine("Произошла непредвиденная ошибка. Попробуйте ещё раз.");
+            }
+
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine("Вы сделали что-то не так. Попробуйте по другому.");
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка");
+            }
+
+            finally
+            {
+                Console.Read();
+            }
+        }
+    }
+
+    public class MyException : Exception
+    {
+        public MyException() { }
+
+        public MyException(string message) : base() { }
+    }
+}
